95f75fd7cdb64d96d31a3aa3d15ddaf76b20fe2b
Ezedike-egwom Collins <ezedikecollins@gmail.com>
2023-10-13 00:37:13 +0100

    auto commit on 2023-10-13 at 00:37:13.297491

--- watcher.py
+++ watcher.py
@@ -29,6 +29,7 @@ class Watcher(FileSystemEventHandler):
     def on_any_event(self, event):
         if event.event_type not in ["opened", "closed"] \
             and self.__is_valid_watch_path(event.src_path):
+            print(event.src_path)
             self.__log(event)
             self.__special_func()
     
